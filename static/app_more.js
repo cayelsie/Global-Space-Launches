cb1e1cbc1f2a0b1ce8aa28dd6ca9898b48a1991f
unknown <cayelsie@gmail.com>
2021-04-26 22:00:48 -0400

    added a bar graph for first two decades

--- static/app_more.js
+++ static/app_more.js
@@ -2,14 +2,11 @@
 var url = "data/global_space_launches.json";
 
 //Create a function for creating the dropdown menu - call it so the user can see the menu initially
-function dropdown() {
+function dropdownCountry() {
 
     // Set a variable to hold an empty array to collect unique country names
 
     countries = ["All"]
-
-    // //****Set variable for mission status array
-    // status_mission = []
 
 
     // set dropdown menu to variable
@@ -28,118 +25,234 @@ function dropdown() {
         });
         console.log(countries);
 
-
-    //****Read file and push status to the empty array
-     //Reads json data file: "data" encompasses the entire thing
-    // d3.json(url).then((data) => {
-
-    //     //Loop through the data to create a new array of status
-    //     data.map(row => {
-    //         var status = row.status_mission;
-
-    //         //If the status isn't already present, push it to the array
-    //         if (!status_mission.includes(status)) {
-    //             status_mission.push(status);
-    //         }
-    //     });
-    //     console.log(status);
-
         //Loop through the country names and add to dropdownMenu
         countries.forEach(name => {
             dropdownMenu.append("option").text(name).property("value");
         });
-      
-        //Loop through the status names and add to dropdownMenu
-        //    status_mission.forEach(name => {
-        //     dropdownMenu.append("option").text(name).property("value");
-        // });
 
     });
 };
-dropdown();
+
+//Create a function for creating the dropdown menu - call it so the user can see the menu initially
+function dropdownStatus() {
+
+    // Set a variable to hold an empty array to collect unique country names
+
+    status_mission = ["All"]
+
+
+    // set dropdown menu to variable
+    var dropdownMenu = d3.select("#selMission");
+    //Reads json data file: "data" encompasses the entire thing
+    d3.json(url).then((data) => {
+
+        //Loop through the data to create a new array of countries
+        data.map(row => {
+            var status = row.status_mission;
+
+            //If the country isn't already present, push it to the array
+            if (!status_mission.includes(status)) {
+                status_mission.push(status);
+            }
+        });
+        console.log(status_mission);
+
+
+        //****Read file and push status to the empty array
+        //Reads json data file: "data" encompasses the entire thing
+        // d3.json(url).then((data) => {
+
+        //     //Loop through the data to create a new array of status
+        //     data.map(row => {
+        //         var status = row.status_mission;
+
+        //         //If the status isn't already present, push it to the array
+        //         if (!status_mission.includes(status)) {
+        //             status_mission.push(status);
+        //         }
+        //     });
+        //     console.log(status);
+
+        // Loop through the status names and add to dropdownMenu
+        status_mission.forEach(name => {
+            dropdownMenu.append("option").text(name).property("value");
+        });
+
+    });
+};
+dropdownCountry();
+dropdownStatus();
 
 buildChart(countries[0]);
+buildEarlyDecades();
 
-//Function for on change/user selection
+
+//Function for on change/user selection of country
 function optionChanged(new_country) {
 
     buildChart(new_country);
 };
 
-    
+//Function for on change/user selection of country
+function nextoptionChanged(new_status) {
+
+    statusChange(new_status);
+};
+
+
+function statusChange(_new_status) {
+    buildChart();
+    console.log(new_status);
+
+};
+
+
+
 
 //Everything to build the state/private chart is encompassed here - if I use filter_data it's controlled by dropdown; use data and it's all data
 function buildChart(new_country) {
 
-//Read the json file   
-d3.json(url).then((data) => {
-    console.log(data);
+    //Read the json file   
+    d3.json(url).then((data) => {
+        console.log(data);
 
-    //Filter main dataset for the selection from the dropdown
-        var filter_data = data.filter(item => {
+        // Filter main dataset for the selection from the country dropdown
+        var filter_country = data.filter(item => {
             return item.country === new_country;
         });
-        console.log(filter_data);
+        console.log(filter_country);
 
-    //Setting a variable for the grouped JS object (by year), using the groupBy function from util.js, with a conditional to use ALL data if all is selected
-    if (new_country === "All") {
-        var groupby_year = groupBy(data, "year");
-    }
+        // Setting a variable for the grouped JS object (by year), using the groupBy function from util.js, with a conditional to use ALL data if all is selected
+        if (new_country === "All") {
+            var groupby_year = groupBy(data, "year");
+        }
 
-    else {
-    var groupby_year = groupBy(filter_data, "year");
-    }
-    console.log(groupby_year);
+        else {
+            var groupby_year = groupBy(filter_country, "year");
+        }
+        console.log(groupby_year);
 
-    //Setting a variable for a empty JS object that will hold my years as main key with S and P as sub-keys with their counts as values
-    //Use the countBy function from util.js to get the counts and then add to the empty JS object
-    var count_by_PS_year = {};
-    Object.entries(groupby_year).forEach(([key, value]) => {
-        var count_by_PS = countBy(value, "private_state");
-        count_by_PS_year[key] = count_by_PS;
+
+
+
+        //Setting variable for grouped JS object with conditionals for status dropdown
+        //     if (new_status === "All") {
+        //     var groupby_year = groupBy(data, "year");
+        // }
+
+        // else {
+        //     var groupby_year = groupBy(filter_status, "year");
+        // }
+        // console.log(groupby_year);
+
+
+        //Setting a variable for a empty JS object that will hold my years as main key with S and P as sub-keys with their counts as values
+        //Use the countBy function from util.js to get the counts and then add to the empty JS object
+        var count_by_PS_year = {};
+        Object.entries(groupby_year).forEach(([key, value]) => {
+            var count_by_PS = countBy(value, "private_state");
+            count_by_PS_year[key] = count_by_PS;
+        });
+        console.log(count_by_PS_year);
+
+        //Getting arrays for the graphing:
+
+        //To get the x-axis I just need the keys from my JS object I created after my counting (which are the years)
+        var x_axis = Object.keys(count_by_PS_year);
+        console.log(x_axis);
+
+        //To get the two y-axes, use the get_display_data function from util.js. 
+        var y_axis_P = get_display_data(count_by_PS_year, "P");
+        var y_axis_S = get_display_data(count_by_PS_year, "S");
+        console.log(y_axis_P);
+        console.log(y_axis_S);
+
+        //Plotly code for the private/state line graph: need two traces for two lines
+        var trace1 = {
+            x: x_axis,
+            y: y_axis_P,
+            type: 'scatter',
+            name: 'Privately Funded'
+        };
+        var trace2 = {
+            x: x_axis,
+            y: y_axis_S,
+            type: 'scatter',
+            name: 'State Funded'
+        };
+
+
+        var layout = {
+            xaxis: { range: [1957, 2020] },
+            yaxis: {
+                range: [0, 110],
+                title: "Total Launches"
+            },
+            title: "Global Space Launches from 1959 - August 2020"
+        };
+
+        var data = [trace1, trace2];
+        Plotly.newPlot('trendline', data, layout);
+
     });
-    console.log(count_by_PS_year);
+};
 
-    //Getting arrays for the graphing:
 
-    //To get the x-axis I just need the keys from my JS object I created after my counting (which are the years)
-    var x_axis = Object.keys(count_by_PS_year);
-    console.log(x_axis);
+function buildEarlyDecades() {
+    //Read the json file   
+    d3.json(url).then((data) => {
 
-    //To get the two y-axes, use the get_display_data function from util.js. 
-    var y_axis_P = get_display_data(count_by_PS_year, "P");
-    var y_axis_S = get_display_data(count_by_PS_year, "S");
-    console.log(y_axis_P);
-    console.log(y_axis_S);
+        // Filter main dataset for the decades, only successful launches
+        var filter_decade = data.filter(item => {
+            return item.year <= 1980;
+        });
 
-    //Plotly code for the private/state line graph: need two traces for two lines
-    var trace1 = {
-        x: x_axis,
-        y: y_axis_P,
-        type: 'scatter',
-        name: 'Privately Funded'
-    };
-    var trace2 = {
-        x: x_axis,
-        y: y_axis_S,
-        type: 'scatter',
-        name: 'State Funded'
-    };
+        var status_filter = filter_decade.filter(item => {
+            return item.status_mission === "Success";
+        });
 
-    
-    var layout = {
-        xaxis: {range: [1957, 2020]},
-        yaxis: {range: [0, 110],
-                title: "Total Launches"},
-        title: "Global Space Launches from 1959 - August 2020"
-    };
+//This allowed me to console log the total  number of successful missions so I could have the number
+        var groupby_status = groupBy(status_filter, "status_mission");
+        console.log(groupby_status);
 
-    var data = [trace1, trace2];
-    Plotly.newPlot('trendline', data, layout);
+//Filter for USA and Russia only
+        var filter_country = status_filter.filter(item => {
+            return item.company_country === "USA" || item.company_country === "Russia";
+        });
+        console.log (filter_country);
 
-});
+//Group by the countries and then logging gives the count of each in the array
+        var groupby_companyCountry = groupBy(status_filter, "company_country");
+        console.log(groupby_companyCountry);
+
+//Hard coding the numbers total successful minus USA and Russia
+        var others = (1615 - (1165 + 399));
+
+//Hard coding x and y axes for bar graph
+        var x = ["Russia", "USA", "Others"];
+        var y = ["1165", "399", others];
+      
+
+           //Plotly code for the bar graph Russia and US
+           var trace = [
+               {
+            x: x,
+            y: y,
+            type: 'bar'
+        }
+    ];
+
+        var layout = {
+            xaxis: { title: "Funding Country" },
+            yaxis: {title: "Total Successful Launches"},
+            title: "Successful Global Space Launches, 1960-1980"
+        };
+
+        Plotly.newPlot('early', trace, layout);
+
+
+    });
+
 }
-
-
 
 
